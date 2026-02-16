cf6f9d6fcceb708dd2ec8ff26122669b3dc6c568
Joonas3 <157369778+Joonas3@users.noreply.github.com>
2026-02-16 12:34:38 +0200

    Update tarkistakoodi.py

--- tarkistakoodi.py
+++ tarkistakoodi.py
@@ -6,9 +6,9 @@ try:
         for i, line in enumerate(file):
             if "TODO" in line:
                 print(f"VIRHE rivillä {i+1}: TODO löydetty!")
-                sys.exit(1) # Punainen valo CI-putkessa
+                sys.exit(1)  # Punainen valo CI-putkessa
 
     print("Koodi puhdas.")
-    sys.exit(0) # Vihreä valo
+    sys.exit(0)  # Vihreä valo
 except FileNotFoundError:
     print("Tiedostoa koodi.txt ei löytynyt.")
