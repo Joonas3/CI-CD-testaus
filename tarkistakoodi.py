b40f4a1f01f523a389d3a52810af050b8ca5aea4
Joonas3 <157369778+Joonas3@users.noreply.github.com>
2026-02-16 13:03:04 +0200

    Update tarkistakoodi.py

--- tarkistakoodi.py
+++ tarkistakoodi.py
@@ -2,7 +2,7 @@ import sys
 
 # Luetaan tiedosto rivi riviltä
 try:
-    with open('jotainkoodia.txt', 'r', encoding='utf-8') as file:
+    with open('jotaintekstiä.txt', 'r', encoding='utf-8') as file:
         for i, line in enumerate(file):
             if "TODO" in line:
                 print(f"VIRHE rivillä {i+1}: TODO löydetty!")
@@ -11,4 +11,4 @@ try:
     print("Koodi puhdas.")
     sys.exit(0)  # Vihreä valo
 except FileNotFoundError:
-    print("Tiedostoa koodi.txt ei löytynyt.")
+    print("Tiedostoa jotaintekstiä.txt ei löytynyt.")
